84e42bd46ac750cf16a236bbf01da0cafe937d2a
”名前” <>
2020-04-27 09:01:30 +0000

    rails課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: {maximum: 15}
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: {minimum: 8,maximum: 32}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
   
   has_secure_password
 end
